ced496ec977aba045d4bf0fe3cf3cc70f7e4aeb6
nassimblddd <nassim.belaid@aiesec.com>
2024-11-01 20:43:59 +0100

    TABLEAUX

--- tableaux.js
+++ tableaux.js
@@ -26,3 +26,33 @@ const girls = ["Lyne", "Kenza", "Alice"];
 
 const myChildren = boys.concat(girls);
 console.log(myChildren, "My Children");
+
+// Using the splice method
+const fruits = ["Banane", "Pomme", "Cerise", "Poire"];
+
+// Inserting elements at index 2
+fruits.splice(2, 0, "Citron", "Kiwi");
+console.log(fruits, "Fruits");
+
+// Removing elements at index 3 and 4
+const fr = fruits.splice(3, 2);
+console.log(fr);
+console.log(fruits);
+
+const fruit2 = ['Banane', 'Orange', 'Citron', 'Pomme', 'Mongue'];
+const SliceFruit = fruit2.slice(2);
+console.log(SliceFruit);
+console.log(fruit2);
+
+const fruit3 = ['Banane', 'Orange', 'Citron', 'Pomme', 'Mongue'];
+const SliceFruit3 = fruit3.slice(1,3);
+console.log(SliceFruit3);
+
+const nombres = [1,2,3,4,5,6,7];
+// Afficher de 1 jusqu'a 6
+
+nombres.splice(6);
+nombres.slice(0, 6);
+
+const troisquatre= nombres.slice(2,4);
+console.log(troisquatre);
